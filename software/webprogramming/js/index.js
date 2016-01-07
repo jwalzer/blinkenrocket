86c195cfbcb4b4a11fda6275a12911ce9f4d6d81
Sebastian Muszytowski <sebastian@muszytowski.net>
2016-01-07 08:47:33 +0100

    first prototype web-IDE layout

--- software/webprogramming/js/index.js
+++ software/webprogramming/js/index.js
@@ -1,6 +1,14 @@
 function changeColor(dot){
   dot.classList.toggle("red");
-  console.log(dot);
+  exportMatrix();
+}
+
+function exportMatrix(){
+  for (var i=0;i<8;i++) { 
+    for (var j=0;j<8;j++) {
+      console.log($("#r"+i+"c"+j)[0].classList.contains("red"));
+    }
+  }
 }
 
 $(document).ready(function() {
@@ -8,7 +16,7 @@ $(document).ready(function() {
   // add dots
   for (var i=0;i<8;i++) { 
     for (var j=0;j<8;j++) {
-      $("#container").append("<a class='cl' id="+"r"+i+"c"+j+" onclick='changeColor(this);'></a>");
+      $("#dotmatrixcontainer").append("<a class='cl' id="+"r"+i+"c"+j+" onclick='changeColor(this);'></a>");
     }
   }
   
